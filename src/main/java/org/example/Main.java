df381629107661e856ca3eda42ef14e759436278
Joel Grant <joelmgrant@me.com>
2022-11-04 12:16:59 -0600

    Add basic menu

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,12 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Welcome to the Movie Tracker!");
+
+        System.out.println("Please enter the number of the selection you wish to make.");
+        System.out.println("1. Add a new movie to your tracker.");
+        System.out.println("2. View movies in your tracker.");
+
+
     }
 }
