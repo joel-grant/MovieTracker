20fe4294b26dca9074bb329042e5379d0cc058ed
Joel Grant <joelmgrant@me.com>
2022-11-04 17:23:09 -0600

    refactor main class

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -12,7 +12,7 @@ public class Main {
     private static final int ADD_MOVIE = 1;
     private static final int VIEW_ALL_MOVIES = 2;
     private static final int QUIT = 3;
-    
+
     /**
      *
      * Driver of the main program.
@@ -21,42 +21,56 @@ public class Main {
      */
     public static void main(String[] args) {
         Scanner userInput = new Scanner(System.in);
-        String response = "";
+        int response = 0;
         System.out.println("Welcome to the Movie Tracker!");
 
         MovieList movies = new MovieList();
 
-        while (!response.equals("3")) {
-            System.out.println("Please enter the number of the selection you wish to make.");
-            System.out.println("1. Add a new movie to your tracker.");
-            System.out.println("2. View movies in your tracker.");
-            System.out.println("3. Quit Program.");
+        while (response != QUIT) {
+            displayMenu();
 
-            response = userInput.next();
+            response = userInput.nextInt();
+            userInput.nextLine();
 
-            if (response.equals("1")) {
-                Movie newMovie = new Movie();
-                System.out.println("Enter the name of the movie");
-                newMovie.setMovieName(userInput.next());
-                userInput.nextLine();
-                System.out.println("Did you watch the movie?");
-                newMovie.setMovieWatched(userInput.next());
+            System.out.println();
 
-                System.out.println(newMovie.getMovieName());
-                System.out.println(newMovie.getTimesWatched());
-                System.out.println(newMovie.getMovieWatched());
-                movies.addNewMovie(newMovie);
+            switch (response) {
+                case ADD_MOVIE:
+                    addMovie(movies, userInput);
+                    break;
+                case VIEW_ALL_MOVIES:
+                    movies.getAllMovies();
+                    break;
+                default:
+                    break;
             }
-            else if (response.equals("2")) {
-                movies.getAllMovies();
-            }
-
         }
+    }
 
+    /**
+     * Facilitates the user input to capture the movie details from the user.
+     *
+     * @param moviesList An Array of Movie objects
+     * @param userInput From the user's keyboard.
+     */
+    private static void addMovie(MovieList moviesList, Scanner userInput) {
+        Movie newMovie = new Movie();
 
+        System.out.println("Enter the name of the movie");
+        newMovie.setMovieName(userInput.nextLine());
+//                userInput.nextLine();
+        System.out.println("Did you watch the movie?");
+        newMovie.setMovieWatched(userInput.next());
+        moviesList.addNewMovie(newMovie);
+    }
 
-
-
-
+    /**
+     * Displays basic text output for the user to see the menu options.
+     */
+    private static void displayMenu() {
+        System.out.println("Please enter the number of the selection you wish to make.");
+        System.out.println(ADD_MOVIE + ". Add a new movie to your tracker.");
+        System.out.println(VIEW_ALL_MOVIES + ". View movies in your tracker.");
+        System.out.println(QUIT + ". Quit Program.");
     }
 }
