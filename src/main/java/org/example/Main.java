199e129740ad0481addece48c7a0844f9eed9ea6
Joel Grant <joelmgrant@me.com>
2022-11-04 14:22:10 -0600

    set up basic menu quit loop

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -17,31 +17,36 @@ public class Main {
      */
     public static void main(String[] args) {
         Scanner userInput = new Scanner(System.in);
-
+        String response = "";
         System.out.println("Welcome to the Movie Tracker!");
 
-        System.out.println("Please enter the number of the selection you wish to make.");
-        System.out.println("1. Add a new movie to your tracker.");
-        System.out.println("2. View movies in your tracker.");
+        while (!response.equals("3")) {
+            System.out.println("Please enter the number of the selection you wish to make.");
+            System.out.println("1. Add a new movie to your tracker.");
+            System.out.println("2. View movies in your tracker.");
+            System.out.println("3. Quit Program.");
 
-        int response = userInput.nextInt();
+            response = userInput.next();
 
-        if (response == 1) {
+            if (response.equals("1")) {
 //            System.out.print("WOMP WOMP");
-            Movie newMovie = new Movie();
-            System.out.println("Enter the name of the movie");
-            newMovie.setMovieName(userInput.next());
-            userInput.nextLine();
-            System.out.println("Did you watch the movie?");
-            newMovie.setMovieWatched(userInput.next());
+                Movie newMovie = new Movie();
+                System.out.println("Enter the name of the movie");
+                newMovie.setMovieName(userInput.next());
+                userInput.nextLine();
+                System.out.println("Did you watch the movie?");
+                newMovie.setMovieWatched(userInput.next());
 
-            System.out.println(newMovie.getMovieName());
-            System.out.println(newMovie.getTimesWatched());
-            System.out.println(newMovie.getMovieWatched());
+                System.out.println(newMovie.getMovieName());
+                System.out.println(newMovie.getTimesWatched());
+                System.out.println(newMovie.getMovieWatched());
+            }
+
         }
 
 
-        
+
+
 
 
     }
