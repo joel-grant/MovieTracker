de5e7843ed4cfc8ae6ef6f09d49b1f42c505d165
Joel Grant <joelmgrant@me.com>
2022-11-04 17:28:44 -0600

    Comment functions

--- src/main/java/org/example/MovieList.java
+++ src/main/java/org/example/MovieList.java
@@ -1,15 +1,30 @@
 package org.example;
 
+/**
+ * MovieLists are lists of Movie objects that facilitates the CRUD process of lists of movies.
+ *
+ * @author Joel Grant
+ * @version 1.0
+ */
 public class MovieList {
-    private static final int LIST_SIZE = 1;
-    private Movie[] movies;
-    private int numberOfMovies;
+    private static final int LIST_SIZE = 1; // Default size of the list is 1.
+    private Movie[] movies; // the default list of movie objects
+    private int numberOfMovies; //
 
+    /**
+     * Default Constructor for a movie list object as a list of Movie objects.
+     */
     public MovieList() {
         this.movies = new Movie[LIST_SIZE];
         this.numberOfMovies = 0;
     }
 
+    /**
+     * Facilitates adding a new movie object to the MovieList array.
+     *
+     * @param movie object
+     * @return true or false
+     */
     public boolean addNewMovie(Movie movie) {
         expandList();
         this.movies[numberOfMovies] = movie;
@@ -18,12 +33,20 @@ public class MovieList {
         return wasAdded;
     }
 
+    /**
+     * Getter method to iterate through all of the movies on the list.
+     */
     public void getAllMovies() {
         for (int i = 0; i < movies.length -1; i++) {
             System.out.println(movies[i].getMovieName());
         }
     }
 
+    /**
+     * Each time something is added to the list, the list is expanded via this method.
+     *
+     * @return true/false if the list was "expanded".
+     */
     private boolean expandList() {
         boolean wasExpanded = false;
 
