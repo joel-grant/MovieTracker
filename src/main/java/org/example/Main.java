35bd250b0e5c61aaf46e3521e8e4a8295723f775
Joel Grant <joelmgrant@me.com>
2022-11-04 13:51:45 -0600

    Create basic movie object and input

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,47 @@
 package org.example;
+import java.util.Scanner;
 
+/**
+ * The main class for the entire program.  Handles the menu and user inputs.
+ *
+ * @author Joel Grant
+ * @version 1.0 2022-Nov-04 Initial brute force version.
+ *
+ */
 public class Main {
+    /**
+     *
+     * Driver of the main program.
+     *
+     * @param args the command line arguments
+     */
     public static void main(String[] args) {
+        Scanner userInput = new Scanner(System.in);
+
         System.out.println("Welcome to the Movie Tracker!");
 
         System.out.println("Please enter the number of the selection you wish to make.");
         System.out.println("1. Add a new movie to your tracker.");
         System.out.println("2. View movies in your tracker.");
+
+        int response = userInput.nextInt();
+
+        if (response == 1) {
+//            System.out.print("WOMP WOMP");
+            Movie newMovie = new Movie();
+            System.out.println("Enter the name of the movie");
+            newMovie.setMovieName(userInput.next());
+            userInput.nextLine();
+            System.out.println("Did you watch the movie?");
+            newMovie.setMovieWatched(userInput.next());
+
+            System.out.println(newMovie.getMovieName());
+            System.out.println(newMovie.getTimesWatched());
+            System.out.println(newMovie.getMovieWatched());
+        }
+
+
+        
 
 
     }
